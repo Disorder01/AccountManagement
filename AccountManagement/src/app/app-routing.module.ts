fff1d475b5b5390c2cb7625bd848077d4090f2d9
Lars Krämer <kraemerlars01@gmail.com>
2025-05-21 20:06:43 +0200

    routing

--- AccountManagement/src/app/app-routing.module.ts
+++ AccountManagement/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { OverviewComponent } from './overview/overview.component';
+import { AdministrationComponent } from './administration/administration.component';
+import { CreateAccountComponent } from './create-account/create-account.component';
+import { UserInfoComponent } from './user-info/user-info.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'overview', component: OverviewComponent },
+  { path: 'administration', component: AdministrationComponent },
+  { path: 'new-account', component: CreateAccountComponent },
+  { path: 'user', component: UserInfoComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
