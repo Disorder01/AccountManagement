fb7a51a3a84d55c949d29c4bc512239b38cfc5af
Lars Krämer <kraemerlars01@gmail.com>
2025-05-26 20:17:07 +0200

    prepare backend

---
+++ Backend/server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const bodyParser = require('body-parser');
+const app = express();
+const PORT = 3000;
+
+app.use(bodyParser.json());
+
+app.post('/api/register', (req, res) => {
+  const user = req.body;
+  const filePath = path.join(__dirname, 'registrations.csv');
+
+  const csvLine = `${user.firstname},${user.lastname},${user.email}\n`;
+
+  const header = 'firstname,lastname,email\n';
+
+  if (!fs.existsSync(filePath)) {
+    fs.writeFileSync(filePath, header);
+  }
+
+  fs.appendFileSync(filePath, csvLine);
+
+  res.json({ success: true });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server läuft auf http://localhost:${PORT}`);
+});
