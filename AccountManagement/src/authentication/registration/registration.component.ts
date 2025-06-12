9cd43429b9453fe7599c3e709ac47358ea227d0f
Lars Krämer <kraemerlars01@gmail.com>
2025-06-12 19:03:59 +0200

    imrpove login and registration

--- AccountManagement/src/authentication/registration/registration.component.ts
+++ AccountManagement/src/authentication/registration/registration.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+// registration.component.ts
+import { Component, OnInit } from '@angular/core';
 import { User } from '../../models/user';
 import { ApiService } from '../../services/apiService';
 import { ValidationService } from '../../services/validationService';
@@ -8,39 +9,52 @@ import { ValidationService } from '../../services/validationService';
   templateUrl: './registration.component.html',
   styleUrls: ['./registration.component.scss']
 })
-export class RegistrationComponent {
-
-  user: User = {
-    id: 0,
-    firstName: '',
-    lastName: '',
-    password: '',
-    accounts: []
-  };
-
+export class RegistrationComponent implements OnInit {
+  user: User = { id: 0, firstName: '', lastName: '', password: '', accounts: [] };
   confirmPassword: string = '';
+  validationErrors: Record<string, string> = {};
+  // neue Property: speichert, welche Felder bereits einmal fokussiert wurden
+  touchedFields: Record<string, boolean> = {};
 
-  validationErrors: {
-    lastName?: string;
-    firstName?: string;
-    password?: string;
-    confirmPassword?: string;
-  } = {};
+  constructor(
+    private apiService: ApiService,
+    private validationService: ValidationService
+  ) {}
 
-  constructor(private apiService: ApiService, private validationService: ValidationService) {}
+  ngOnInit(): void {}
 
-  createUser() {
-    this.validationErrors = this.validationService.validateUser(this.user, this.confirmPassword);
+  /** wird aufgerufen, sobald ein Input den Fokus erhält */
+  onFieldFocus(field: string): void {
+    this.touchedFields[field] = true;
+  }
 
-    if (Object.keys(this.validationErrors).length > 0) {
-      // Fehler vorhanden, abbrechen
+  /**
+   * Prüft das Formular über den ValidationService und befüllt validationErrors.
+   */
+  isFormValid(): boolean {
+    this.validationErrors = this.validationService.validateUser(
+      this.user,
+      this.confirmPassword
+    );
+    return Object.keys(this.validationErrors).length === 0;
+  }
+
+  /**
+   * Sendet die Daten nur, wenn das Formular valide ist.
+   */
+  createUser(): void {
+    if (!this.isFormValid()) {
       return;
     }
 
-    this.apiService.registerNewUser(this.user)
-      .subscribe({
-        next: () => alert('Profil erfolgreich erstellt!'),
-        error: (err: any) => alert('Fehler bei der Erstellung: ' + err.message)
-      });
+    this.apiService.registerNewUser(this.user).subscribe({
+      next: () => {
+        alert('Profil erfolgreich erstellt!');
+      },
+      error: err => {
+        console.error('Fehler beim Erstellen des Profils', err);
+        alert('Es ist ein Fehler aufgetreten. Bitte erneut versuchen.');
+      }
+    });
   }
 }
