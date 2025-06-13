3ad02fd2f17d8b5cafedc7c831dc13b010ad5d7e
Lars Krämer <kraemerlars01@gmail.com>
2025-06-13 19:03:07 +0200

    welcome-page

--- AccountManagement/src/app/overview/overview.component.ts
+++ AccountManagement/src/app/overview/overview.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { User } from '../../models/user';
+import { AuthService } from '../AuthService';
 
 @Component({
   selector: 'app-overview',
   templateUrl: './overview.component.html',
   styleUrl: './overview.component.scss'
 })
-export class OverviewComponent {
+export class OverviewComponent implements OnInit {
+  user: User | null | undefined;
 
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {
+    this.user = this.authService.currentUser;
+  }
 }
